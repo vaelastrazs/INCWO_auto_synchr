574d6f1232fbe8a9ff7264482ec5f1015c269b27
vaelastrazs <felix.bernacki@gmail.com>
2016-09-10 19:02:18 +0200

    test to utf8

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from lxml import etree
 
-catalog_actual =  etree.parse("incwo_catalog.xml")
+catalog_actual = etree.parse("incwo_catalog.xml")
 products_actual = catalog_actual.getroot()
 for product in catalog_actual.xpath("/customer_products/customer_product"):
-    print product

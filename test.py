3416496fc31e5c4872da297db03451f85608d96a
vaelastrazs <felix.bernacki@gmail.com>
2016-09-10 18:27:10 +0200

    test

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from lxml import etree
 
 catalog_actual =  etree.parse("incwo_catalog.xml")
 products_actual = catalog_actual.getroot()
-for product in products_actual:
+for product in catalog_actual.xpath("/customer_products/customer_product"):
     print product

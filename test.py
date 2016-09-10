f0c00aea506556449740a8068fd46b3f699448b7
vaelastrazs <felix.bernacki@gmail.com>
2016-09-10 19:08:13 +0200

    Test getting picata attribut

--- test.py
+++ test.py
@@ -3,3 +3,4 @@ from lxml import etree
 catalog_actual = etree.parse("incwo_catalog.xml")
 products_actual = catalog_actual.getroot()
 for product in catalog_actual.xpath("/customer_products/customer_product"):
+	print "Ref : ",product.find('Référence'), " | cat : ",product.find('Catégorie')," | Constructeur : ",product.find('Constructeur')," | Prix HT : ",product.find('Px_HT')

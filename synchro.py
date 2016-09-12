55ba2e74244f96ad3faa98641be6a624408ea5ba
vaelastrazs <felix.bernacki@gmail.com>
2016-09-12 19:27:49 +0200

    y'en a qui bosse meme le lundi!

--- synchro.py
+++ synchro.py
@@ -25,38 +25,43 @@ f.close()
 print "catalog incwo has currently ", count," items"
 
 
-foreach (catalog_fourniseur->customer_product as product) {
-	found = false;
-	reference_fourniseur = product->Référence; #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+
+for product in catalog_actual.xpath("/customer_products/customer_product"):
+    found = False
+	reference_fourniseur = product->Référence; 		#TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+	prix_fourniseur = product->Px_HT; 				#TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+	marque_fourniseur = product->Constructeur; 		#TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+	categorie_fourniseur = product->Catégorie; 		#TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+	stock_fourniseur = product->Stock_Dispo_Achard; #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+	cmd_fourniseur = product->En_cde_Achard; 		#TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+	
 	i = 0;
-	foreach (catalog_actual->customer_product as actual_product) {
+	foreach (catalog_actual->customer_product as actual_product):
 		
-		reference_incwo = actual_product->reference;
+		reference_incwo = actual_product->reference
 		#echo "incwo : reference_incwo </br>";
-		if ((int) reference_fourniseur == (int) reference_incwo){
-			#echo "modifiying product id ".actual_product->id." \n";
-			found = true;
-			if (cross_check[i]){
-				echo "Warning : doublon pour produit ".actual_product->id." \n";
-			}
-			cross_check[i] = 1;
-			update_product(product, actual_product);
-			break;
-		}
-		i++;
+		if (reference_fourniseur == reference_incwo):
+			#echo "modifiying product id ".actual_product->id." \n"
+			found = True;
+			if (cross_check[i]):
+				print "Warning : doublon pour produit ".actual_product->id
+			cross_check[i] = 1
+			update_product(product, actual_product)
+			break
+		i++
 	}
 	if (!found){
-		echo "create new producte for reference reference_fourniseur \n";
+		print "create new producte for reference reference_fourniseur"
 		
-		create_new_product(product);
+		#create_new_product(product)
 	}
 }
-for (i = 0 ; i < count(cross_check) ; i ++ ){
-	if (!cross_check[i]){
-		echo "remove unused product \n";
-		delete_product(catalog_actual->customer_product[i]->id);
-	}
-}
+for (i = 0 ; i < count(cross_check) ; i ++ ):
+	if (!cross_check[i]):
+		print "remove unused product with id : ",catalog_actual->customer_product[i]->id
+		#delete_product(catalog_actual->customer_product[i]->id)
+	
+
 
 
 ?>

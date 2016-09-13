7575fb67013dd6b6df7261c4986e9242cf1455f7
vaelastrazs <felix.bernacki@gmail.com>
2016-09-14 01:02:16 +0200

    :: ;_;

--- myLib.py
+++ myLib.py
@@ -27,24 +27,24 @@ def update_product(product, actual_product):
     #Useless, for debug purpose    
     for child in product:
         print(child.tag)
-        if child.tag == "Référence".decode('utf-8')::
+        if child.tag == "Référence".decode('utf-8'):
             reference_fourniseur = child.text	
-        if child.tag == "Px_HT".decode('utf-8')::
+        if child.tag == "Px_HT".decode('utf-8'):
             prix_fourniseur = child.text
-        if child.tag == "Constructeur".decode('utf-8')::
+        if child.tag == "Constructeur".decode('utf-8'):
             marque_fourniseur = child.text
-        if child.tag == "Catégorie".decode('utf-8')::
+        if child.tag == "Catégorie".decode('utf-8'):
             categorie_fourniseur = child.text
-        if child.tag == "Stock_Dispo_Achard".decode('utf-8')::
+        if child.tag == "Stock_Dispo_Achard".decode('utf-8'):
             stock_fourniseur = child.text
-        if child.tag == "En_cde_Achard".decode('utf-8')::
+        if child.tag == "En_cde_Achard".decode('utf-8'):
             cmd_fourniseur = child.text
             
     for child in actual_product:
         print(child.tag)
-        if child.tag == "id".decode('utf-8')::
+        if child.tag == "id".decode('utf-8'):
             id_incwo = child.text	#TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-        if child.tag == "cost".decode('utf-8')::
+        if child.tag == "cost".decode('utf-8'):
             cout_incwo = child.text
         # if child.tag == "Stock_Dispo_Achard":
         #     stock_incwo = child.text

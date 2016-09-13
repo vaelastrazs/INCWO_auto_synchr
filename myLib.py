c6034d936a4ad6e35ffe40f2c481d4723a1aff47
vaelastrazs <felix.bernacki@gmail.com>
2016-09-14 01:00:34 +0200

    ...almost?

--- myLib.py
+++ myLib.py
@@ -8,14 +8,14 @@ import requests
 def get_incwo_brand(id):
     with open('marques.txt', 'r') as fp:
         for line in fp:
-            datas = line.splite(":")
+            datas = line.split(":")
             if datas[0] == id:
                 return datas[1]
             
 def get_incwo_categories(id):
     with open('categories.txt', 'r') as fp:
         for line in fp:
-            datas = line.splite(":")
+            datas = line.split(":")
             if datas[0] == id:
                 return datas[1]
             
@@ -27,24 +27,24 @@ def update_product(product, actual_product):
     #Useless, for debug purpose    
     for child in product:
         print(child.tag)
-        if child.tag == "Référence":
+        if child.tag == "Référence".decode('utf-8')::
             reference_fourniseur = child.text	
-        if child.tag == "Px_HT":
+        if child.tag == "Px_HT".decode('utf-8')::
             prix_fourniseur = child.text
-        if child.tag == "Constructeur":
+        if child.tag == "Constructeur".decode('utf-8')::
             marque_fourniseur = child.text
-        if child.tag == "Catégorie":
+        if child.tag == "Catégorie".decode('utf-8')::
             categorie_fourniseur = child.text
-        if child.tag == "Stock_Dispo_Achard":
+        if child.tag == "Stock_Dispo_Achard".decode('utf-8')::
             stock_fourniseur = child.text
-        if child.tag == "En_cde_Achard":
+        if child.tag == "En_cde_Achard".decode('utf-8')::
             cmd_fourniseur = child.text
             
     for child in actual_product:
         print(child.tag)
-        if child.tag == "id":
+        if child.tag == "id".decode('utf-8')::
             id_incwo = child.text	#TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-        if child.tag == "cost":
+        if child.tag == "cost".decode('utf-8')::
             cout_incwo = child.text
         # if child.tag == "Stock_Dispo_Achard":
         #     stock_incwo = child.text

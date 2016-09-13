9e2df3cd126cbc6931b789b8ffc58349b0beb28c
vaelastrazs <felix.bernacki@gmail.com>
2016-09-13 19:29:33 +0200

    tab to space*4

--- lib.py
+++ lib.py
@@ -30,19 +30,18 @@ def update_product(product, actual_product):
 
     #Useless, for debug purpose    
     reference = product->Référence
-
-	prix_fourniseur = product->Px_HT 				    #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-	marque_fourniseur = product->Constructeur 		    #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-	categorie_fourniseur = product->Catégorie 		    #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-	stock_fourniseur = product->Stock_Dispo_Achard      #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-	cmd_fourniseur = product->En_cde_Achard             #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+    prix_fourniseur = product->Px_HT 				    #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+    marque_fourniseur = product->Constructeur 		    #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+    categorie_fourniseur = product->Catégorie 		    #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+    stock_fourniseur = product->Stock_Dispo_Achard      #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+    cmd_fourniseur = product->En_cde_Achard             #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
     
     id_incwo = actual_product->id
-	cout_incwo = actual_product->cost			        #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-	marque_incwo = get_incwo_brand(actual_product->brand_id)
-	categorie_incwo =  get_incwo_categories(actual_product->product_category_id)		
-	#stock_incwo = actual_product->   #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-	#cmd_incwo = actual_product->          #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+    cout_incwo = actual_product->cost			        #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+    marque_incwo = get_incwo_brand(actual_product->brand_id)
+    categorie_incwo =  get_incwo_categories(actual_product->product_category_id)		
+    #stock_incwo = actual_product->   #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
+    #cmd_incwo = actual_product->          #TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
     
     print "Ref produit : ", reference
     

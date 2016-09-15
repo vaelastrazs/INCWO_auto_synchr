4709d9a9e8b63a4f2ac146f0260ae1cf47f032b2
vaelastrazs <felix.bernacki@gmail.com>
2016-09-15 19:17:15 +0200

    test past request

--- test.py
+++ test.py
@@ -7,12 +7,12 @@ import myLib
 catalog_actual = etree.parse("picata_catalog.xml")
 products_actual = catalog_actual.getroot()
 i =0
-for product in catalog_actual.xpath("/customer_products/customer_product"):
-	print "Ref : ",product.find('Référence'.decode('utf-8')).text, " | cat : ",product.find('Catégorie'.decode('utf-8')).text," | Constructeur : ",product.find('Constructeur'.decode('utf-8')).text," | Prix HT : ",product.find('Px_HT'.decode('utf-8')).text
-	i+=1
-	if i==100:
-		break
-	
+# for product in catalog_actual.xpath("/customer_products/customer_product"):
+# 	print "Ref : ",product.find('Référence'.decode('utf-8')).text, " | cat : ",product.find('Catégorie'.decode('utf-8')).text," | Constructeur : ",product.find('Constructeur'.decode('utf-8')).text," | Prix HT : ",product.find('Px_HT'.decode('utf-8')).text
+# 	i+=1
+# 	if i==100:
+# 		break
+# 	
 xml_data="<customer_product><reference>123456</reference>\
 		<is_active>1</is_active>\
 		<is_from_vendor>0</is_from_vendor>\

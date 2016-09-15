7627c687681917dedcbac0e76b3867047af2d18d
vaelastrazs <fbernacki@tb.nce.equant.com>
2016-09-15 13:28:26 +0200

    test.break

--- test.py
+++ test.py
@@ -5,5 +5,9 @@ from lxml import etree
 
 catalog_actual = etree.parse("picata_catalog.xml")
 products_actual = catalog_actual.getroot()
+i =0
 for product in catalog_actual.xpath("/customer_products/customer_product"):
 	print "Ref : ",product.find('Référence'.decode('utf-8')).text, " | cat : ",product.find('Catégorie'.decode('utf-8')).text," | Constructeur : ",product.find('Constructeur'.decode('utf-8')).text," | Prix HT : ",product.find('Px_HT'.decode('utf-8')).text
+	i+=1
+	if i==100:
+		break

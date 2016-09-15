5ccd9b5bca2da0f61201e40f455a009e138765fc
Vaelastrazs <synchro@sd-91432.dedibox.fr>
2016-09-15 13:15:09 +0200

    mise a jour gitignore

--- myLib.py
+++ myLib.py
@@ -44,8 +44,8 @@ def update_product(product, actual_product):
         print(child.tag)
         if child.tag == "id".decode('utf-8'):
             id_incwo = child.text	#TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-        if child.tag == "cost".decode('utf-8'):
-            cout_incwo = child.text
+        if child.tag == "price".decode('utf-8'):
+            price_incwo = child.text
         # if child.tag == "Stock_Dispo_Achard":
         #     stock_incwo = child.text
         # if child.tag == "En_cde_Achard":
@@ -60,5 +60,5 @@ def update_product(product, actual_product):
         ", stock : ",stock_fourniseur,", en commande : ", cmd_fourniseur)
     
     print("INCWO :")
-    print("cout : ", cout_incwo,", marque : ", marque_incwo,", categorie :",categorie_incwo)
+    print("cout : ", price_incwo,", marque : ", marque_incwo,", categorie :",categorie_incwo)
 

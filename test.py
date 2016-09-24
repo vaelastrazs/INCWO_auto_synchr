85294707d970bfa220781b5914eeaacc18b7be43
vaelastrazs <felix.bernacki@gmail.com>
2016-09-24 18:51:56 +0200

    test ->raz_bdd

--- test.py
+++ test.py
@@ -8,9 +8,6 @@ import re
 import myLib
 import time
 
-catalog_fourniseur = etree.parse("picata_catalog.xml")
-products_fourniseur = catalog_fourniseur.getroot()
-print("catalog picata loaded")
 catalog_actual =  etree.parse("incwo_catalog.xml")
 products_actual = catalog_actual.getroot()
 print("catalog incwo loaded")
@@ -19,39 +16,15 @@ count = catalog_actual.xpath('count(//customer_product)')
 cross_check = [False] * int(count)
 print("catalog incwo has currently ", count," items")
 
+threads = []
 
-
-for product in catalog_fourniseur.findall("./customer_product"):
-    found = False
-    fournisseur_datas = myLib.get_fournisseur_product_infos(product)
-    if not 'reference' in fournisseur_datas:
-        print("produit sans ref, skipping...")
-        continue
-    # for child in product:
-    #     if child.tag == "Référence".decode('utf-8'):
-    #         reference_fourniseur = child.text #.decode('iso-8859-15').encode('utf8')	#TOIMPROVE Depend du CSV recuperer, a mettre en parametrable
-    #         break
-    i = 0
-    for actual_product in catalog_actual.findall("./customer_product") :
-        
-        reference_incwo = myLib.get_incwo_ref(actual_product)
-        if not reference_incwo:
-            print("produit incwo sans ref, skipping...")
-            #myLib.delete_current_product()
-        elif fournisseur_datas['reference'] == reference_incwo:
-            print("reference incwo found!")
-            found = True
-            if cross_check[i]:
-                print("Warning : doublon pour produit ",actual_product)
-            cross_check[i] = True
-            incwo_datas = myLib.get_incwo_product_infos(actual_product)
-            myLib.update_product(fournisseur_datas, incwo_datas)
-            break
-        i+=1
-    if not found:
-        print("create new producte for reference reference_fourniseur")
-        myLib.create_product(fournisseur_datas)
-    time.sleep(1)
+for myID in catalog_actual.findall("./customer_product/id") :
+    r = myLib.myRequester("delete",myID, None)
+    r.start()
+    threads.append(r)
+    
+for t in threads:
+    t.join
 # 	
 # xml_data="<customer_product><reference>123456</reference>\
 # 		<is_active>1</is_active>\

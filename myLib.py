facf0845f89b621c4a162985d6688f5ef3e0bcc7
vaelastrazs <felix.bernacki@gmail.com>
2016-11-02 16:00:51 +0100

    Je suis debile

--- myLib.py
+++ myLib.py
@@ -204,7 +204,7 @@ class myRequester(Thread):
         headers = {'content-type': 'application/xml'}
         rc = 0
         retry = 0
-        while (rc != 200 or retry < 3):
+        while (rc != 200 and retry < 3):
             pool_sema.acquire()
             retry += 1
             if self.method == "get":

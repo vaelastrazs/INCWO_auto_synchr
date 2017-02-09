b3e124a9f83c669189fb92baf48d43ca8c4b58c6
vaelastrazs <felix.bernacki@gmail.com>
2017-02-09 16:39:30 +0100

    Modif stock_mov.xml pour correspondre avec ceux d'INCWO

--- myLib.py
+++ myLib.py
@@ -162,7 +162,6 @@ def prepare_xml_product(product_infos):
 def prepare_xml_stock_movement(warehouse_id, quantity, product_id):
     xml_data="<stock_movement>\
              <customer_product_id>"+product_id+"</customer_product_id>\
-             <destination_warehouse_id>"+str(warehouse_id)+"</destination_warehouse_id>\
              <origin_warehouse_id>"+str(warehouse_id)+"</origin_warehouse_id>\
              <quantity>"+str(quantity)+"</quantity>\
              <direction>0</direction>\

99e407416d2eeb954cc334b86a4e6e369be8b257
vaelastrazs <felix.bernacki@gmail.com>
2017-04-22 18:32:58 +0200

    titi

--- myLib.py
+++ myLib.py
@@ -281,9 +281,10 @@ def update_product(fournisseur_product_infos, incwo_product_infos):
         if not key in fournisseur_product_infos:
             log.error("Product "+fournisseur_product_infos["name"]+" : fournisseur info incomplete! Missing "+key)
             raise ValueError()
-        elif not key in incwo_product_infos and key != "barcode":
-            log.debug("incwo info incomplete, updating "+key)
-            update_infos[key]=fournisseur_product_infos[key]
+        elif not key in incwo_product_infos:
+            if key != 'barcode':
+                log.debug("incwo info incomplete, updating "+key)
+                update_infos[key]=fournisseur_product_infos[key]
         elif (compareValues(fournisseur_product_infos[key],incwo_product_infos[key])):
             log.debug("incwo info outdated, updating "+key)
             log.debug("Picata "+str(fournisseur_product_infos[key])+" ; incwo_product_infos "+str(incwo_product_infos[key]))

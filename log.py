35e73c2e4d5b261dfaa8ed26cae35819b29b0d5d
bhackus <antoningp@clic-ordi.com>
2017-05-21 15:10:02 +0200

    create the path for logfiles if not exist
    create the path for logfiles if not exist

--- log.py
+++ log.py
@@ -1,35 +1,39 @@
 from time import strftime
 import logging
+import os
 
 
 PATH = "logs" # EDITABLE : change for your own convenience
 filename = strftime("%Y%m%d_%H")
 LOG_LEVEL = logging.INFO # EDITABLE : change for your own convenience
 
+if not os.path.exists(PATH):
+    os.makedirs(PATH)
+            
+if (os.path.exists(PATH)):
+    # create logger with 'spam_application'
+    logger = logging.getLogger('Incwo_Updater')
+    logger.setLevel(LOG_LEVEL)
+    # create file handler which logs even debug messages
 
-# create logger with 'spam_application'
-logger = logging.getLogger('Incwo_Updater')
-logger.setLevel(LOG_LEVEL)
-# create file handler which logs even debug messages
+    fh = logging.FileHandler(PATH+"/"+filename+'.log')
+    fh.setLevel(LOG_LEVEL)
+    # create formatter and add it to the handlers
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
 
-fh = logging.FileHandler(PATH+"/"+filename+'.log')
-fh.setLevel(LOG_LEVEL)
-# create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fh.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(fh)
+    def error(error_msg):
+        logger.error(error_msg)
 
-def error(error_msg):
-    logger.error(error_msg)
+    def warning(msg):
+        logger.warning(msg)    
 
-def warning(msg):
-    logger.warning(msg)    
+    def info(msg):
+        logger.info(msg)
     
-def info(msg):
-    logger.info(msg)
-    
-def debug(debug_msg):
-    logger.debug(debug_msg)
+    def debug(debug_msg):
+        logger.debug(debug_msg)
 
 
